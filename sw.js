0d75568f0733881343a2e6feaef2ec4b07fcbba6
jlvFlores <jluisvazquez.jv@gmail.com>
2022-10-06 15:23:18 -0500

    62. cache dinamico - optimizaciones

--- sw.js
+++ sw.js
@@ -1,9 +1,13 @@
 const CACHE_NAME = 'cache-1';
+const CACHE_STATIC_NAME  = 'static-v1';
+const CACHE_DYNAMIC_NAME = 'dynamic-v1';
+
+const CACHE_INMUTABLE_NAME = 'inmutable-v1';
 
 
 self.addEventListener('install', e => {
     
-    const cacheProm = caches.open(CACHE_NAME).then( cache => {
+    const cacheProm = caches.open(CACHE_STATIC_NAME).then( cache => {
 
         return cache.addAll([
             '/',
@@ -16,7 +20,10 @@ self.addEventListener('install', e => {
         
     });
 
-    e.waitUntil(cacheProm);
+    const cacheInmutable = caches.open( CACHE_INMUTABLE_NAME)
+        .then( cache => cache.add('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'));
+
+    e.waitUntil(Promise.all([cacheProm, cacheInmutable]));
 });
 
 self.addEventListener('fetch', e => {
@@ -29,7 +36,7 @@ self.addEventListener('fetch', e => {
             console.log('No existe', e.request.url);
 
             fetch( e.request ).then( newResp => {
-                caches.open(CACHE_NAME).then( cache => {
+                caches.open(CACHE_DYNAMIC_NAME).then( cache => {
                     cache.put(e.request, newResp);
                 });
 
