9eba060d0c3a77d6f61fb34ae2760fc6841b161a
jlvFlores <jluisvazquez.jv@gmail.com>
2022-10-06 16:05:45 -0500

    66. cache y network race

--- sw.js
+++ sw.js
@@ -25,7 +25,8 @@ self.addEventListener('install', e => {
             '/css/style.css',
             '/img/main.jpg',
             'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
-            '/js/app.js'
+            '/js/app.js',
+            '/img/no-img.jpg'
         ]);
         
     });
@@ -38,18 +39,48 @@ self.addEventListener('install', e => {
 
 self.addEventListener('fetch', e => {
 
-    // 4- Cache with networks update
-    if(e.request.url.includes('bootstrap')) {
-        return e.respondWith(caches.match(e.request));
-    }
-    
-    const respuesta = caches.open( CACHE_STATIC_NAME ).then( cache => {
-        fetch(e.request).then( newRes => cache.put(e.request,newRes));
-        
-        return cache.match(e.request);
+    // 5- Cache & Network Race
+    const respuesta = new Promise( (resolve, reject) => {
+
+        let rechazada = false;
+
+        const falloUnaVez = () => {
+            if(rechazada) {
+                if(/\.(png|jpg)$/i.test(e.request.url)) {
+                    resolve(caches.match('/img/no-image.jpg'));
+                } else {
+                    reject('No se encontro respuesta');
+                }
+            } else {
+                rechazada = true;
+            }
+        };
+
+        fetch(e.request).then( res => {
+            res.ok ? resolve(res) : falloUnaVez();
+        }).catch(falloUnaVez);
+
+        caches.match(e.request).then( res => {
+            res ? resolve(res) : falloUnaVez();
+        }).catch(falloUnaVez);
+
     });
 
     e.respondWith(respuesta);
+
+
+    // 4- Cache with networks update
+    // if(e.request.url.includes('bootstrap')) {
+    //     return e.respondWith(caches.match(e.request));
+    // }
+    
+    // const respuesta = caches.open( CACHE_STATIC_NAME ).then( cache => {
+    //     fetch(e.request).then( newRes => cache.put(e.request,newRes));
+        
+    //     return cache.match(e.request);
+    // });
+
+    // e.respondWith(respuesta);
 
 
     // 3- Network with cache fallback
