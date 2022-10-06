010d3779f102bdcf8a6bf200266a5021fc9ea3a2
jlvFlores <jluisvazquez.jv@gmail.com>
2022-10-06 15:52:32 -0500

    65. cache with network update

--- sw.js
+++ sw.js
@@ -38,22 +38,38 @@ self.addEventListener('install', e => {
 
 self.addEventListener('fetch', e => {
 
-    // 3- Network with cache fallback
-    const respuesta = fetch( e.request ).then( res => {
-        if( !res ) return caches.match( e.request );
+    // 4- Cache with networks update
+    if(e.request.url.includes('bootstrap')) {
+        return e.respondWith(caches.match(e.request));
+    }
+    
+    const respuesta = caches.open( CACHE_STATIC_NAME ).then( cache => {
+        fetch(e.request).then( newRes => cache.put(e.request,newRes));
         
-        console.log('Fetch', res);
-        caches.open( CACHE_DYNAMIC_NAME ).then( cache => {
-            cache.put( e.request, res );
-            limpiarCache( CACHE_DYNAMIC_NAME, CACHE_DYNAMIC_LIMIT);
-        });
-
-        return res.clone();
-    }).catch( err => {
-        return caches.match( e.request );
+        return cache.match(e.request);
     });
 
     e.respondWith(respuesta);
+
+
+    // 3- Network with cache fallback
+    // const respuesta = fetch( e.request ).then( res => {
+    //     if( !res ) return caches.match( e.request );
+        
+    //     console.log('Fetch', res);
+    //     caches.open( CACHE_DYNAMIC_NAME ).then( cache => {
+    //         cache.put( e.request, res );
+    //         limpiarCache( CACHE_DYNAMIC_NAME, CACHE_DYNAMIC_LIMIT);
+    //     });
+
+    //     return res.clone();
+    // }).catch( err => {
+    //     return caches.match( e.request );
+    // });
+
+    // e.respondWith(respuesta);
+
+    
     // 2- Cache with Network Fallback
     // const respuesta = caches.match( e.request )
     //     .then( res => {
@@ -72,6 +88,7 @@ self.addEventListener('fetch', e => {
     //     });
 
     // e.respondWith(respuesta);
+
 
     // 1- Cache Only
     // e.respondWith( caches.match( e.request ) );
