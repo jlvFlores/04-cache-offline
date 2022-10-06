cd1988fd8a449d28d87e6ed2c6468bcec42c50ab
jlvFlores <jluisvazquez.jv@gmail.com>
2022-10-06 13:42:57 -0500

    59. guardar el app shell a la hora de instalar sw

--- sw.js
+++ sw.js
@@ -1,39 +1,17 @@
-self.addEventListener('fetch', event => {
 
-    // const offlineResp = new Response(`
-    //     Bienvenido a mi pagina web
+self.addEventListener('install', e => {
+    
+    const cacheProm = caches.open('cache-1').then( cache => {
+
+        return cache.addAll([
+            '/index.html',
+            '/css/style.css',
+            '/img/main.jpg',
+            'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+            '/js/app.js'
+        ]);
         
-    //     ocupas internet
-    // `);
+    });
 
-    // const offlineResp = new Response(`
-    
-    // <!DOCTYPE html>
-    // <html lang="en">
-    // <head>
-    //     <meta charset="UTF-8">
-    //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    //     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    //     <title>Mi PWA</title>
-    
-    // </head>
-    // <body class="container p-3">
-
-    // <h1>Offline Mode</h1>
-
-    // </body>
-    // </html>
-
-    // `, {
-    //     headers: {
-    //         'Content-Type':'text/html'
-    //     }
-    // });
-
-    const offlineResp = fetch( 'pages/offline.html' );
-
-    const resp = fetch(event.request).catch( () => offlineResp);
-
-    event.respondWith( resp );
-
+    e.waitUntil(cacheProm);
 });
